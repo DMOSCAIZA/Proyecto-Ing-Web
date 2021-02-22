5fb833ec27abecd598576193bb7d8d73fc64cde0
Diego Moscaiza <diegomoscaiza@outlook.com>
2021-02-22 16:00:54 -0500

    Primer Commit

---
+++ Frontend-Vue/src/views/dashboard/art/ventas/Productos.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container
+    id="regular-tables"
+    fluid
+    tag="section"
+  >
+    <base-v-component
+      heading="Simple Tables"
+      link="components/simple-tables"
+    />
+
+    <base-material-card
+      icon="mdi-clipboard-text"
+      title="Simple Table"
+      class="px-5 py-3"
+    >
+      <v-simple-table>
+
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Descripción</th>
+            <th>XD</th>
+            <th>XD</th>
+            <th>XD</th>
+            <th>XD</th>
+            <th>XD</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="producto in productos" :key="producto.id">
+
+          </tr>
+        </tbody>
+
+      </v-simple-table>
+    </base-material-card>
+
+  </v-container>
+</template>
+
