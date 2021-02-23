40756b97557a3fb896ffd001d11b4d91e2ba41f3
Diego Moscaiza <diegomoscaiza@outlook.com>
2021-02-23 14:58:36 -0500

    Declaración del puerto usado en mi computadora con "XAMPP" (Diego)

--- Backend-PHP/conexion.php
+++ Backend-PHP/conexion.php
@@ -4,10 +4,14 @@
             define('servidor', 'localhost');
             define('nombre_bd', 'heladeria');
             define('usuario', 'root');
-            define('password', '');	
-            $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
+            define('password', '');
+            define('puerto','33065');
+            $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'utf8\'');			
             try{
-                $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);
+                $conexion = new PDO(
+                    "mysql:host=".servidor.
+                    "; port=".puerto.
+                    "; dbname=".nombre_bd, usuario, password, $opciones);
                 return $conexion;                    
             }catch (Exception $e){
                 die("El error de Conexión es: ". $e->getMessage());
