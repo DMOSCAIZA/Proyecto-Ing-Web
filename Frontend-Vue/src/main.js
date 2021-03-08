2590f736c85cbde001b187d1b8cf174b1e42b489
Diego Moscaiza <diegomoscaiza@outlook.com>
2021-03-08 13:39:26 -0500

    Añadiendo ruta en 'Axios'

--- Frontend-Vue/src/main.js
+++ Frontend-Vue/src/main.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 Vue.config.productionTip = false
 
-axios.defaults.baseURL='http://localhost/1-PROYECTOS/PROYECTO%20WEB/DESARROLLO/Backend-PHP/',
+axios.defaults.baseURL='http://localhost/1-PROYECTOS/PROYECTO%20WEB/DESARROLLO/Proyecto-Ing-WEB/Frontend-Vue/public/Backend-PHP/',
 
 new Vue({
   router,
